1fce30098e6c075429bf1e0ccaaf6f64b7caadee
Kossi Fioklou <kossifioklou10@gmail.com>
2022-08-05 07:26:19 -0700

    Fix linters

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>ok</h1>
+    <h1>Del Consult</h1>
   </React.StrictMode>,
 );
