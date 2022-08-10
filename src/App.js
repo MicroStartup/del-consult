3bd5de18a35e4f1a19b211696956ea13a15fe6c7
codecaiine <akabrouyannickn@gmail.com>
2022-08-10 22:25:06 +0000

    Import navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-center">Del consult</h1>
+      <Navbar />
     </>
   );
 }
