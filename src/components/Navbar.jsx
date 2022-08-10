95a2242cbf66f7f0498130f2ebcdb5dc48d6e546
codecaiine <akabrouyannickn@gmail.com>
2022-08-10 22:38:03 +0000

    Add navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
 
 const Navbar = () => {
   return (
-    <div>
-       Navabae
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
+      <div className='px-2 flex justify-between items-center w-full h-full'>
+        
+      </div>
+       <MenuIcon className='w-5'/> 
     </div>
   )
 }
