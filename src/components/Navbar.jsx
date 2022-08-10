2453cfd9e2ddfaf8899e1b96efab4e8da7bc63e1
codecaiine <akabrouyannickn@gmail.com>
2022-08-10 22:48:16 +0000

    Add menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,14 @@ const Navbar = () => {
       <div className='px-2 flex justify-between items-center w-full h-full'>
          <div className='flex items-center'>
               <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Del Consult</h1>
+         <ul className='hidden md:flex'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Service</li>
+          <li>Project</li>
+          <li>Services</li>
+          <li>Contact</li>
+         </ul>
          </div>
       </div>
        <MenuIcon className='w-5'/> 
