e356ef42ae5f7f9a1a768729a3f812bcd901e514
OLIPLICHE <olipliche@gmail.com>
2022-08-10 16:05:30 +0100

    Create pages folder, service page file, contact page file, update App.js, Update contact and service component

---
+++ src/pages/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Services from '../components/Services';
+import './Services.css';
+
+const ServicesPage = () => (
+  <section className="service-page">
+    <h1 className="services-headline">Service page is under construction</h1>
+    <div className="services-content">
+      <Services />
+    </div>
+  </section>
+);
+
+export default ServicesPage;
