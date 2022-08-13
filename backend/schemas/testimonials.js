5930811386f2fbd548dad243a15125c8d7bba6e7
codecaiine <akabrouyannickn@gmail.com>
2022-08-13 20:05:01 +0000

    Create basic testimonial schema structure

--- backend/schemas/testimonials.js
+++ backend/schemas/testimonials.js
@@ -8,5 +8,23 @@ export default{
             title: 'Name',
             type: 'string'
         },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
     ]
 }
