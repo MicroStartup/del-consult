9098f8adb2d776c60ee9f48ba5baf86c41d6741b
codecaiine <akabrouyannickn@gmail.com>
2022-08-13 21:14:27 +0000

    Finalize  project schema

---
+++ backend/schemas/projects.js
@@ -0,0 +1,50 @@
+export default {
+    name: 'projects',
+    title: 'Projects',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+    
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+      {
+        name: 'companyName',
+        title: 'Company Name',
+        type: 'string',
+      },
+      {
+        name: 'companyLink',
+        title: 'Company Link',
+        type: 'string',
+      },
+      {
+        name: 'imgUrl',
+        title: 'ImageUrl',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+   
+      {
+        name: 'tags',
+        title: 'Tags',
+       type:'array',
+       of: [
+         {
+           name:'tag',
+           title:'Tag',
+           type:'string'
+         }
+       ]
+      },
+     
+    ],
+  };
