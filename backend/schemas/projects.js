4ae1530ba0f0bc7a42a6dd6b40bd0d0d9d60cc82
codecaiine <akabrouyannickn@gmail.com>
2022-08-13 22:22:56 +0000

    Modify project schema

--- backend/schemas/projects.js
+++ backend/schemas/projects.js
@@ -4,8 +4,8 @@ export default {
     type: 'document',
     fields: [
       {
-        name: 'title',
-        title: 'Title',
+        name: 'projectTitle',
+        title: 'Project Title',
         type: 'string',
       },
     
