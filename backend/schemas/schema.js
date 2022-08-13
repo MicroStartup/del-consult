012bd9df6526b9fd8efdcc6b12969aa15c22159f
codecaiine <akabrouyannickn@gmail.com>
2022-08-13 21:15:42 +0000

    Import project schema

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import testimonials from './testimonials'
 import abouts from './abouts'
 import contact from './contact'
+import projects from './projects'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -16,6 +17,7 @@ export default createSchema({
   types: schemaTypes.concat([
     testimonials,
     abouts,
-    contact
+    contact,
+    projects
   ]),
 })
