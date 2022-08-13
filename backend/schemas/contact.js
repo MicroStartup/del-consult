89819175dc4e88dead2cb56d81bf1b3d155239bc
codecaiine <akabrouyannickn@gmail.com>
2022-08-13 21:01:41 +0000

    Finalie contact schema

--- backend/schemas/contact.js
+++ backend/schemas/contact.js
@@ -1,3 +1,37 @@
-export default{
-    
+export default {
+    name:'contact',
+    title:'Contact',
+    type:'document',
+    fields:[
+        {
+            name:'firstname',
+            title:'Fistname',
+            type:'string'
+        },
+        {
+            name:'lastname',
+            title:'Lastname',
+            type:'string'
+        },
+        {
+            name:'gender',
+            title:'Gender',
+            type:'string'
+        },
+        {
+            name:'phone',
+            title:'Phone',
+            type:'string'
+        },
+        {
+            name:'email',
+            title:'Email',
+            type:'string'
+        },
+        {
+            name:'message',
+            title:'Message',
+            type:'text'
+        }
+    ]
 }
