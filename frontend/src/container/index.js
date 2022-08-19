89428260fdf83cc4bcbdeeceea20055bb2968ebd
codecaiine <akabrouyannickn@gmail.com>
2022-08-19 22:03:22 +0000

    Export all my container variable

---
+++ frontend/src/container/index.js
@@ -0,0 +1,7 @@
+export { default as About } from './About/About';
+export { default as Contact } from './Contact/Contact';
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as Project } from './Project/Project';
+export { default as Services } from './Services/Services';
+export { default as Testamonial } from './Testamonial/Testamonial';
