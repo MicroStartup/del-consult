e3bc5f318a5f998af6f4395f6a661e0047a3962b
codecaiine <akabrouyannickn@gmail.com>
2022-08-19 22:58:20 +0000

    Add log navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,7 +5,11 @@ import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>Navbar
+       <div>
+          <img src={images.logo} alt="Fake logo" />
+       </div>
+    </nav>
   )
 }
 
