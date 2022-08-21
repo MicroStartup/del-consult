9466ed5a96321c7f7c10014005477eddc6ed985c
codecaiine <akabrouyannickn@gmail.com>
2022-08-21 21:23:35 +0000

    Fix image

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,4 +1,4 @@
 import logo from '../assets/logo.png'
 import civil_engineering from '../assets/civil-engineering.jpg';
 
-export default { logo };
+export default { logo, civil_engineering };
--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.civil-engineering} alt="civil engineering bg" />
+      <img src={images.civil_engineering} alt="civil engineering bg" />
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
