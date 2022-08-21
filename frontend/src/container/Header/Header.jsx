d45096ab507d215ee51b2109b5ecdd6ed099f196
codecaiine <akabrouyannickn@gmail.com>
2022-08-21 20:56:12 +0000

    Add header structure

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -12,7 +12,21 @@ const Header = () => {
         transition={{ duration: 0.5 }}
         className="app__header-info"
       >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋</span>
+          <div className="comp_intro">
+            <h1 className="head-text">Del consult</h1>
+            <p className="p-text">
+            At BAE Systems, our advanced defence technology protects people and national security, and keeps critical information and infrastructure secure. We search for new ways to provide our customers with a competitive edge across the air, maritime, land and cyber domains. We employ a skilled workforce of 90,500 people in more than 40 countries, and work closely with local partners to support economic development by transferring knowledge, skills and technology.
+            </p>
+          </div>
+        </div>
 
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Accra east legon </p>
+        </div>
+      </div>
       </motion.div>
     </div>
   )
