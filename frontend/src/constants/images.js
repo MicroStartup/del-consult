4b0f2d9e5c9cac7ac749e7deedadf116962c3488
codecaiine <akabrouyannickn@gmail.com>
2022-08-22 16:18:52 +0000

    Add text, animation to header

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,4 +1,5 @@
 import logo from '../assets/logo.png'
 import civil_engineering from '../assets/civil-engineering.jpg';
+import engineer from '../assets/Engineer.png';
 
-export default { logo, civil_engineering };
+export default { logo, civil_engineering, engineer };
