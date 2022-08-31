fcf89f69ee786b25e6add82608acef9a7936c002
codecaiine <akabrouyannickn@gmail.com>
2022-08-31 01:17:21 +0000

    Remove old frontend

--- frontend/src/constants/images.js
+++
@@ -1,6 +0,0 @@
-import logo from '../assets/logo.png'
-import civil_engineering from '../assets/civil-engineering.jpg';
-import engineer from '../assets/Engineer.png';
-import redux from '../assets/redux.png';
-
-export default { logo, civil_engineering, engineer, redux };
