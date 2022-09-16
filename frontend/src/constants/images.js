6fa8f0cb86c816f746e050f2cea3616fe810ff5a
OLIPLICHE <olipliche@gmail.com>
2022-09-16 06:40:37 +0100

    Update image file

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -6,6 +6,11 @@ import archi_design from '../assets/archi_design.jpg';
 import architecture from '../assets/architecture.jpg';
 import design from '../assets/design.jpg';
 import estimation from '../assets/estimation.JPG';
+import firstp from '../assets/firstp.JPG';
+import lastp from '../assets/lastp.JPG';
+import secondp from '../assets/secondp.JPG';
+import thirdp from '../assets/thirdp.JPG';
+
 
 export default {
     building,
@@ -16,4 +21,8 @@ export default {
     architecture,
     design,
     estimation,
+    thirdp,
+    secondp,
+    lastp,
+    firstp,
 };
