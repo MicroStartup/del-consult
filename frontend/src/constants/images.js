329d27fce638a0ab9c0eb1aa595db39fca62d15f
OLIPLICHE <olipliche@gmail.com>
2022-09-16 04:22:50 +0100

    Update images

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -2,10 +2,18 @@ import building from '../assets/building.jpg';
 import construction from '../assets/construction.jpg';
 import engineer from '../assets/engineer.jpg';
 import engineer_material from '../assets/engineer-material.png';
+import archi_design from '../assets/archi_design.jpg';
+import architecture from '../assets/architecture.jpg';
+import design from '../assets/design.jpg';
+import estimation from '../assets/estimation.JPG';
 
 export default {
     building,
     construction,
     engineer,
     engineer_material,
+    archi_design,
+    architecture,
+    design,
+    estimation,
 };
