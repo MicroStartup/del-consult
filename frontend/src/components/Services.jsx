297bf28910161a720a319846fce5368d88ce0d9f
Kossi Fioklou <kossifioklou10@gmail.com>
2022-09-17 11:18:52 -0700

    Replace the image for each card

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -10,9 +10,10 @@ const Services = () => {
             <div className=''>
                 <h2 className='text-5xl font-bold'>What we provide!</h2>
                 <p className='text-2xl py-6 text-gray-500'>
-                At Del Consult , Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem sed risus ultricies tristique nulla aliquet enim tortor at. Morbi tincidunt augue interdum velit euismod in.
-                Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Viverra tellus in hac habitasse platea dictumst.
+                At Del Consult , we provide our Clients with timeless and modern designs to meet their needs 
+                while ensuring that Net zero carbon is achieved as we ought to combat climate change. Our engineering 
+                services include; Structural designs, retrofitting design of an existing structure, Renovations, Structural 
+                integrity Assessment, underpinning of existing foundation and others
                 </p>
             </div>
         </div>
@@ -21,25 +22,25 @@ const Services = () => {
                     <div className='text-gray-500 flex justify-center'> 
                       <img className='h-48 min-h-0 rounded-md' src={images.architecture} alt="/" />
                     </div>
-                    <p className='text-xl mt-2'>Structural Designs</p>
+                    <p className='text-xl mt-2'>Structural Designs And Retrofitting Design</p>
                 </div>
                 <div  className='border py-8 rounded-xl shadow-xl hover:bg-gray-200' >
                    <div className='text-gray-500 flex justify-center'> 
-                      <img className='h-48 min-h-0 rounded-md' src={images.estimation} alt="/" />
+                      <img className='h-48 min-h-0 rounded-md' src={images.estimations} alt="/" />
                    </div>
-                   <p className='text-xl mt-2'>Quantity Estimations</p>
+                   <p className='text-xl mt-2'>Structural Integrity <br></br>Assessment</p>
                 </div>
                 <div className='border py-8 rounded-xl shadow-xl hover:bg-gray-200' >
                     <div className='text-gray-500 flex justify-center'> 
                       <img className='h-48 min-h-0 rounded-md' src={images.archi_design} alt="/" />
                     </div>
-                    <p className='text-xl mt-2'>Architectural Designs</p>
+                    <p className='text-xl mt-2'>Renovations</p>
                 </div>
                 <div className='border py-8 rounded-xl shadow-xl hover:bg-gray-200' >
                     <div className='text-gray-500 flex justify-center'> 
-                      <img className='h-48 min-h-0 rounded-md' src={images.building} alt="/" />
+                      <img className='h-48 min-h-0 rounded-md' src={images.fifthp} alt="/" />
                     </div>
-                    <p className='text-xl mt-2'>Construction Supervision</p>
+                    <p className='text-xl mt-2'>Underpinning of Existing Foundation and Others</p>
                 </div>
             </div>
     </div>
