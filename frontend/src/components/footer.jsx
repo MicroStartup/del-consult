a0f7c3db64e94fc957865ee50a3f940b10108af1
Kossi Fioklou <kossifioklou10@gmail.com>
2022-10-25 06:45:26 -0700

    Add styling for footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div name='Footer' class="flex flex-row my-8 px-4 text-gray-400 justify-between">
+    <div name='Footer' class="flex flex-col md:flex-row items-center my-8 px-4 text-black justify-between">
         <div>
             <h1 class="">Copyright © 2022 Del Consult</h1>
         </div>
