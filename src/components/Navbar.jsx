80c713d153345c38f8a3efda47af0b3171052d4f
OLIPLICHE <olipliche@gmail.com>
2022-10-31 02:24:20 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,17 +15,19 @@ const Navbar = () => {
   return (
        <div className='w-screen h-[70px] z-10 bg-gray-200 border-b border-white-500 fixed'>
           <div className='px-2 flex justify-between items-center w-full h-full'>
-             <div className='flex items-center'>
-                <img className='h-10 min-h-0 px-4' src={images.logo} alt="Company Logo" />
-                <h1 className='font-bold text-[20px]'>Del Consult</h1>
-                <ul className='hidden md:flex cursor-pointer'>
+                <div className='flex items-center'>
+                  <img className='h-10 min-h-0 px-4' src={images.logo} alt="Company Logo" />
+                  <h1 className='font-bold text-[20px]'>Del Consult</h1>
+                </div>
+                <div className='flex items-center'>
+                  <ul className='hidden md:flex cursor-pointer'>
                     <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
                     <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
                     <li><Link to="service" smooth={true} offset={-50} duration={500}>Services</Link></li>
                     <li><Link to="project" smooth={true} offset={-100} duration={500}>Projects</Link></li>
                     <li><Link to="contact" smooth={true} offset={-50} duration={500}>Contact</Link></li>
-                </ul>
-             </div>
+                  </ul>
+                </div>
             <div className='hidden md:flex pr-4'>
               <button className='px-8 py-3 rounded-md'>Quotation</button>
             </div>
